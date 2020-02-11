994721acb9afcf4e1c940f002910e26dc36fd39c
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-02-12 00:17:45 +0900

    33min 34sec

---
+++ 11727.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <memory.h>
+using namespace std;
+
+int main(void){
+    int N;
+    cin>>N;
+    int DP[N+1];
+    DP[0] = DP[1] = 1;
+    for(int i=2;i<=N;i++){
+        DP[i] = (DP[i-1] + DP[i-2]*2)%10007;
+    }
+    cout<<DP[N]<<endl;
+}
