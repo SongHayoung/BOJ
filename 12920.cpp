056eff20e20cac044c9213b3615d91d0a4677c6b
SongHayoung <lovelydays95@naver.com>
2020-02-14 22:49:04 +0900

    1hour 30min 14sec N이하의 모든 수를 만드는 최소 조합 만들기

--- 12920.cpp
+++ 12920.cpp
@@ -11,8 +11,8 @@ int main(void) {
     scanf("%d %d",&N,&M);
     for(int i=1;i<=N;i++){
         scanf("%d %d %d",&_V,&_C,&_K);
-        for(;_K>0; _K>>=1){
-            multi = _K-(_K>>1);
+        for(;_K>0; _K>>=1){     //_K이하의 모든 수를 만들 수 있는 조합을 최소의 갯수로 만들기
+            multi = _K-(_K>>1); //EX) 9라면 5 2 1 1로 9 이하의 모든 수 표현 가능
             V.push_back(_V*multi);
             C.push_back(_C*multi);
         }
