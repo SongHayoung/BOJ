c3ee44f75b9b04a061b1a9fee74a6619a5dc3b1e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-03-02 00:21:33 +0900

    -

--- 1016.cpp
+++ 1016.cpp
@@ -7,23 +7,14 @@ int main() {
 	bool ssJGS[1000001] = { false, };
 	scanf("%ld %ld", &m1, &m2);
 	register long i, from, answer = m2 - m1 + 1;
-	for (i = 0; i < 3; i++) {
-		for (from = gF(m1, PREV[i]); from <= m2 - m1; from += mul(PREV[i])) {
-			if (!ssJGS[from]) {
-				ssJGS[from] = true;
-				--answer;
-			}
-		}
-	}
+	for (i = 0; i < 3; i++)
+		for (from = gF(m1, PREV[i]); from <= m2 - m1; from += mul(PREV[i]))
+			if (!ssJGS[from]) ssJGS[from] = answer--;
 	for (i = 7; (i*i) <= m2; i += 2) {
 		if (!(i % 3)) continue;
 		if (!(i % 5)) continue;
-		for (from = gF(m1,i); from <= m2 - m1; from += mul(i)) {
-			if (!ssJGS[from]) {
-				ssJGS[from] = true;
-				--answer;
-			}
-		}
+		for (from = gF(m1, i); from <= m2 - m1; from += mul(i))
+			if (!ssJGS[from]) ssJGS[from] = answer--;
 	}
 	printf("%ld", answer);
 }
