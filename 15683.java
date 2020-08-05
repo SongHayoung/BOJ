478a6a1c617dbccec0ddd32609b16d6052a9833a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-08-05 17:31:11 +0900

    20min 43sec

--- 15683.java
+++ 15683.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main {
     static int[][] map = new int[8][8];
-    static boolean[][] v = new boolean[8][8];
+    static int[][] v = new int[8][8];
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
     static cctv[] cctvs = new cctv[8];
@@ -15,8 +15,19 @@ public class Main {
             for(int j = 0; j < m; ++j) {
                 map[i][j] = r.nextInt();
                 if(1 <= map[i][j] && map[i][j] <= 5)
-                    cctvs[cnt++] = new cctv(i, j, 0);
+                    cctvs[cnt++] = new cctv(i, j);
             }
+        for(int i = 0; i < cnt; ++i)
+            if(map[cctvs[i].f][cctvs[i].s] == 5)
+                for(int k = 0; k < 4; ++k) {
+                    int nx = cctvs[i].s + dx[k];
+                    int ny = cctvs[i].f + dy[k];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[k];
+                        nx += dx[k];
+                    }
+                }
 
         bfs(0);
         System.out.println(ans);
@@ -24,121 +35,159 @@ public class Main {
 
     static void bfs(int idx) {
         if(idx == cnt) {
-            solution();
+            int ret = 0;
+            for(int i = 0; i < n && ret < ans && ans != 0; ++i)
+                for(int j = 0; j < m; ++j)
+                    if(v[i][j] == 0 && map[i][j] == 0)
+                        ++ret;
+            ans = Math.min(ans, ret);
             return;
         }
         switch (map[cctvs[idx].f][cctvs[idx].s]) {
             case 1:
-            case 3:
-            case 4:
-                for(cctvs[idx].d = 0; cctvs[idx].d < 4; ++cctvs[idx].d)
+                for(int d = 0; d < 4; ++d) {
+                    int nx = cctvs[idx].s + dx[d];
+                    int ny = cctvs[idx].f + dy[d];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[d];
+                        nx += dx[d];
+                    }
                     bfs(idx + 1);
+                    nx = cctvs[idx].s + dx[d];
+                    ny = cctvs[idx].f + dy[d];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        --v[ny][nx];
+                        ny += dy[d];
+                        nx += dx[d];
+                    }
+                }
                 break;
             case 2:
-                for(cctvs[idx].d = 0; cctvs[idx].d < 2; ++cctvs[idx].d)
+                for(int d = 0; d < 2; ++d) {
+                    int nx = cctvs[idx].s + dx[d];
+                    int ny = cctvs[idx].f + dy[d];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[d];
+                        nx += dx[d];
+                    }
+                    nx = cctvs[idx].s + dx[d + 2];
+                    ny = cctvs[idx].f + dy[d + 2];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[d + 2];
+                        nx += dx[d + 2];
+                    }
                     bfs(idx + 1);
+                    nx = cctvs[idx].s + dx[d];
+                    ny = cctvs[idx].f + dy[d];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        --v[ny][nx];
+                        ny += dy[d];
+                        nx += dx[d];
+                    }
+
+                    nx = cctvs[idx].s + dx[d + 2];
+                    ny = cctvs[idx].f + dy[d + 2];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        --v[ny][nx];
+                        ny += dy[d + 2];
+                        nx += dx[d + 2];
+                    }
+                }
+                break;
+            case 3:
+                for(int d = 0; d < 4; ++d) {
+                    int nx = cctvs[idx].s + dx[d];
+                    int ny = cctvs[idx].f + dy[d];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[d];
+                        nx += dx[d];
+                    }
+                    nx = cctvs[idx].s + dx[(d + 1) % 4];
+                    ny = cctvs[idx].f + dy[(d + 1) % 4];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[(d + 1) % 4];
+                        nx += dx[(d + 1) % 4];
+                    }
+                    bfs(idx + 1);
+                    nx = cctvs[idx].s + dx[d];
+                    ny = cctvs[idx].f + dy[d];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        --v[ny][nx];
+                        ny += dy[d];
+                        nx += dx[d];
+                    }
+                    nx = cctvs[idx].s + dx[(d + 1) % 4];
+                    ny = cctvs[idx].f + dy[(d + 1) % 4];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        --v[ny][nx];
+                        ny += dy[(d + 1) % 4];
+                        nx += dx[(d + 1) % 4];
+                    }
+                }
+                break;
+            case 4:
+                for(int d = 0; d < 4; ++d) {
+                    int nx = cctvs[idx].s + dx[d];
+                    int ny = cctvs[idx].f + dy[d];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[d];
+                        nx += dx[d];
+                    }
+                    nx = cctvs[idx].s + dx[(d + 1) % 4];
+                    ny = cctvs[idx].f + dy[(d + 1) % 4];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[(d + 1) % 4];
+                        nx += dx[(d + 1) % 4];
+                    }
+
+                    nx = cctvs[idx].s + dx[(d + 3) % 4];
+                    ny = cctvs[idx].f + dy[(d + 3) % 4];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        ++v[ny][nx];
+                        ny += dy[(d + 3) % 4];
+                        nx += dx[(d + 3) % 4];
+                    }
+                    bfs(idx + 1);
+                    nx = cctvs[idx].s + dx[d];
+                    ny = cctvs[idx].f + dy[d];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        --v[ny][nx];
+                        ny += dy[d];
+                        nx += dx[d];
+                    }
+                    nx = cctvs[idx].s + dx[(d + 1) % 4];
+                    ny = cctvs[idx].f + dy[(d + 1) % 4];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        --v[ny][nx];
+                        ny += dy[(d + 1) % 4];
+                        nx += dx[(d + 1) % 4];
+                    }
+                    nx = cctvs[idx].s + dx[(d + 3) % 4];
+                    ny = cctvs[idx].f + dy[(d + 3) % 4];
+                    while(0 <= nx && nx < m && 0 <= ny && ny < n && map[ny][nx] != 6) {
+                        --v[ny][nx];
+                        ny += dy[(d + 3) % 4];
+                        nx += dx[(d + 3) % 4];
+                    }
+                }
                 break;
             case 5: bfs(idx + 1);
             default: break;
         }
     }
 
-    static void solution() {
-        int ret = 0;
-        for(int i = 0; i < n; ++i)
-            for(int j = 0; j < m; ++j)
-                v[i][j] = false;
-        for(int i = 0; i < cnt; ++i) {
-            switch (map[cctvs[i].f][cctvs[i].s]) {
-                case 1:
-                case 2:{
-                    int nx = cctvs[i].s + dx[cctvs[i].d];
-                    int ny = cctvs[i].f + dy[cctvs[i].d];
-                    while(0 <= nx && nx < m && 0 <= ny && ny < n) {
-                        if(map[ny][nx] == 6) break;
-                        v[ny][nx] = true;
-                        ny += dy[cctvs[i].d];
-                        nx += dx[cctvs[i].d];
-                    }
-                }
-                if(map[cctvs[i].f][cctvs[i].s] == 1) break;
-                {
-                    int nx = cctvs[i].s + dx[cctvs[i].d + 2];
-                    int ny = cctvs[i].f + dy[cctvs[i].d + 2];
-                    while(0 <= nx && nx < m && 0 <= ny && ny < n) {
-                        if(map[ny][nx] == 6) break;
-                        v[ny][nx] = true;
-                        ny += dy[cctvs[i].d + 2];
-                        nx += dx[cctvs[i].d + 2];
-                    }
-                }
-                break;
-                case 3:
-                case 4:{
-                    int nx = cctvs[i].s + dx[cctvs[i].d];
-                    int ny = cctvs[i].f + dy[cctvs[i].d];
-                    while(0 <= nx && nx < m && 0 <= ny && ny < n) {
-                        if(map[ny][nx] == 6) break;
-                        v[ny][nx] = true;
-                        ny += dy[cctvs[i].d];
-                        nx += dx[cctvs[i].d];
-                    }
-                    nx = cctvs[i].s + dx[(cctvs[i].d + 1) % 4];
-                    ny = cctvs[i].f + dy[(cctvs[i].d + 1) % 4];
-                    while(0 <= nx && nx < m && 0 <= ny && ny < n) {
-                        if(map[ny][nx] == 6) break;
-                        v[ny][nx] = true;
-                        ny += dy[(cctvs[i].d + 1) % 4];
-                        nx += dx[(cctvs[i].d + 1) % 4];
-                    }
-
-                    if(map[cctvs[i].f][cctvs[i].s] == 3) break;
-
-                    nx = cctvs[i].s + dx[(cctvs[i].d + 3) % 4];
-                    ny = cctvs[i].f + dy[(cctvs[i].d + 3) % 4];
-                    while(0 <= nx && nx < m && 0 <= ny && ny < n) {
-                        if(map[ny][nx] == 6) break;
-                        v[ny][nx] = true;
-                        ny += dy[(cctvs[i].d + 3) % 4];
-                        nx += dx[(cctvs[i].d + 3) % 4];
-                    }
-                    break;
-                }
-                case 5: {
-                    for(int j = 0; j < 4; ++j) {
-                        int nx = cctvs[i].s + dx[j];
-                        int ny = cctvs[i].f + dy[j];
-                        while(0 <= nx && nx < m && 0 <= ny && ny < n) {
-                            if(map[ny][nx] == 6) break;
-                            v[ny][nx] = true;
-                            ny += dy[j];
-                            nx += dx[j];
-                        }
-                    }
-                }
-            }
-        }
-        for(int i = 0; i < n; ++i)
-            for(int j = 0; j < m; ++j)
-                if(!v[i][j] && map[i][j] == 0)
-                    ++ret;
-        ans = Math.min(ans, ret);
-    }
-
-    static class pair{
-        public int f, s;
-        public pair(int f, int s){
-            this.f = f;
-            this.s = s;
-        }
-    }
-
     static class cctv{
-        public int f, s, d;
-        public cctv(int f, int s, int d) {
+        public int f, s;
+        public cctv(int f, int s) {
             this.f = f;
             this.s = s;
-            this.d = d;
         }
     }
 
